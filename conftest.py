86890e9590b944b630e886cbe64af78390c6d3ee
Marina Rikkonen <marina.rikkonen@yandex.ru>
2022-09-23 20:09:11 +0300

    add conftest

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
 def browser(request):
     language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': "es"})
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
